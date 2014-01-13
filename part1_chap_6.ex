fae4b0beebdaa063dab015e9d183e50b19d92b2b
Damien Ragoucy <damien.ragoucy@af83.com>
2014-01-14 00:30:40 +0100

    Removed part1 - chap 6 comments

--- part1_chap_6.ex
+++ part1_chap_6.ex
@@ -1,24 +1,23 @@
-################################################################################
 # Find modules
-################################################################################
+# ============
 
 # Convert a float to a string with 2 decimal digits. (Erlang)
-#:io.format("The number is: ~4.2f~n", [3.1415])
-  #|> IO.puts
+:io.format("The number is: ~4.2f~n", [3.1415])
+  |> IO.puts
 
 # Get the value of an operating system environment variable. (Elixir)
-#System.get_env
-  #|> Enum.find(fn x -> elem(x, 0) === "RUBY_VERSION" end)
-  #|> elem(0)
-  #|> IO.puts
+System.get_env
+  |> Enum.find(fn x -> elem(x, 0) === "RUBY_VERSION" end)
+  |> elem(0)
+  |> IO.puts
 
 # Return the extension component of a file name (so return .exs if given "dave/test.exs")
-#"/Users/dam/dev/elixir/tests/dam.exs"
-  #|> Path.extname
-  #|> IO.puts
+"/Users/dam/dev/elixir/tests/dam.exs"
+  |> Path.extname
+  |> IO.puts
 
 # Return the current working directory of the process. (Elixir)
-#System.cwd |> IO.puts
+System.cwd |> IO.puts
 
 # Convert a string containing JSON into Elixir data structures. (Just find, don’t install)
 # https://github.com/cblage/elixir-json
@@ -26,39 +25,45 @@
 # Execute an command in your operating system’s shell
 System.cmd("ls") |> IO.puts
 
-################################################################################
-# Guess
-################################################################################
 
-# defmodule Chap6 do
-#   def guess(number, a..b) do
-#     half = div(b - a, 2) + a
-#     _guess half, number, a, b
-#   end
 
-#   def _guess(half, number, low, high) when half === number do
-#     IO.puts "Number is #{half} - low: #{low}, high: #{high}"
-#   end
-#   def _guess(half, number, low, high) when half > number do
-#     IO.puts "Is it number #{half} - low: #{low}, high: #{high}"
-#     guess number, low..half
-#   end
-#   def _guess(half, number, low, high) when half < number do
-#     IO.puts "Is it number #{half} - low: #{low}, high: #{high}"
-#     guess number, half..high
-#   end
-# end
+defmodule Chap6 do
+  # Guess
+  # =====
+  def guess(number, a..b) do
+    half = div(b - a, 2) + a
+    _guess half, number, a, b
+  end
+  def _guess(half, number, low, high) when half === number do
+    IO.puts "Number is #{half} - low: #{low}, high: #{high}"
+  end
+  def _guess(half, number, low, high) when half > number do
+    IO.puts "Is it number #{half} - low: #{low}, high: #{high}"
+    guess number, low..half
+  end
+  def _guess(half, number, low, high) when half < number do
+    IO.puts "Is it number #{half} - low: #{low}, high: #{high}"
+    guess number, half..high
+  end
+
+  # PGCD
+  # ====
+  def pgcd(m, 0), do: m
+  def pgcd(m, n), do: pgcd(n, rem(m, n))
+
+  # Sum
+  # ===
+
+  def sum(n), do: _sum(n, 0)
+  # Tail recursive
+  def _sum(0, acc), do: acc
+  def _sum(n, acc), do: _sum(n - 1, acc + n)
+
+  def sum_bof(0), do: 0
+  def sum_bof(n), do: n + sum_bof(n - 1)
+end
 
 # Chap6.guess 273, 1..1000
-
-################################################################################
-# PGCD
-################################################################################
-
-# defmodule Chap6 do
-#   def pgcd(m, 0), do: m
-#   def pgcd(m, n), do: pgcd(n, rem(m, n))
-# end
 
 # IO.puts "#{Chap6.pgcd 758, 306}"
 # IO.puts "#{Chap6.pgcd 26, 15}"
@@ -67,19 +72,6 @@ System.cmd("ls") |> IO.puts
 # IO.puts "#{Chap6.pgcd 23987203982587230467029834502873406, 3874569275908374509}"
 
 
-################################################################################
-# Sum
-################################################################################
-
-# defmodule Chap6 do
-#   def sum(n), do: _sum(n, 0)
-#   # Tail recursive
-#   def _sum(0, acc), do: acc
-#   def _sum(n, acc), do: _sum(n - 1, acc + n)
-
-#   def sum_bof(0), do: 0
-#   def sum_bof(n), do: n + sum_bof(n - 1)
-# end
 
 # IO.puts "#{Chap6.sum 5}"
 # IO.puts "#{Chap6.sum_bof 5}"

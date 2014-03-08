2b6c75e5bacbb753c0a5428a923cc19b427c05b6
Damien Ragoucy <damien.ragoucy@af83.com>
2014-03-08 23:16:50 +0100

    Part 2 - Chap 12: two processes

--- part2-chap12.ex
+++ part2-chap12.ex
@@ -15,3 +15,26 @@ defmodule SpawnBasic do
     end
   end
 end
+
+defmodule Spawn do
+  def on_answer do
+    receive do
+      { msg, pid } -> send pid, msg
+    end
+  end
+
+  def send_message msg, pid do
+    send pid, { msg, self }
+    receive do
+      msg_back -> IO.puts msg_back
+    end
+  end
+
+  def two_processes do
+    callback1_pid = spawn Spawn, :on_answer, []
+    callback2_pid = spawn Spawn, :on_answer, []
+
+    spawn Spawn, :send_message, ["Dam", callback1_pid]
+    spawn Spawn, :send_message, ["Plop", callback2_pid]
+  end
+end
